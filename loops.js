3a0bd0dfe7c77d18cd007f7403dffa1c8374dc03
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-12 15:47:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,14 +19,5 @@ function whileLoop(n) {
 }
 
 
-function doWhileLoop(int) {
-var i = 0;
-  function incrementVariable() {
-  i = i + 1;
-  return i;
-  }
-  do {
-    console.log('I run once regardless.')
-  } while (incrementVariable() < num);
-}
+function doWhileLoop(num)
 
